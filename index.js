1e2da1814cdf8899922fc1e5a824066b104edbbe
ifty mahmud <iftymahmud123@gmail.com>
2020-11-02 20:46:22 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+document.querySelector("#k1").addEventListener("click", play1);
+function play1() {
+    var audio = new Audio("./notes/C.mp3");
+    audio.play();
+}
+
+document.querySelector("#k2").addEventListener("click", play2);
+function play2() {
+    var audio = new Audio("./notes/D.mp3");
+    audio.play();
+}
+
+document.querySelector("#k3").addEventListener("click", play3);
+function play3() {
+    var audio = new Audio("./notes/E.mp3");
+    audio.play();
+}
+
+document.querySelector("#k4").addEventListener("click", play4);
+function play4() {
+    var audio = new Audio("./notes/F.mp3");
+    audio.play();
+}
+
+document.querySelector("#k5").addEventListener("click", play5);
+function play5() {
+    var audio = new Audio("./notes/G.mp3");
+    audio.play();
+}
+
+document.querySelector("#k6").addEventListener("click", play6);
+function play6() {
+    var audio = new Audio("./notes/A.mp3");
+    audio.play();
+}
+
+document.querySelector("#k7").addEventListener("click", play7);
+function play7() {
+    var audio = new Audio("./notes/B.mp3");
+    audio.play();
+}
+
+document.querySelector("#k8").addEventListener("click", play8);
+function play8() {
+    var audio = new Audio("./notes/Cn.mp3");
+    audio.play();
+}
